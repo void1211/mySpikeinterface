36a0c668af936d2192475320dabba7fcd6c80753
void1211 <i2586254@cc.kyoto-su.ac.jp>
2026-02-13 18:05:14 +0900

    runners update

--- myTools/output_converter.py
+++ myTools/output_converter.py
@@ -4,8 +4,6 @@ from scipy.io import savemat
 from pathlib import Path
 import torch
 
-from kilosort.io import load_ops
-
 
 def convert_to_matlab_files_ks4(res_dir: Path | str, recording):
     """Kilosort4 出力フォルダ内の主要な .npy/.tsv を .mat にまとめて保存する。
@@ -16,35 +14,38 @@ def convert_to_matlab_files_ks4(res_dir: Path | str, recording):
     Parameters
     ----------
     res_dir : Path | str
-        Kilosort4 の結果フォルダ（`spike_times.npy` などがあるディレクトリ）。
+        結果フォルダ（`spike_times.npy` などがあるディレクトリ）。
     recording : Recording
         SpikeInterface の Recording オブジェクト。
     """
-    res_dir = Path(res_dir)
+    ks_dir = Path(res_dir) / 'sorting' / 'sorter_output'
+    save_dir = Path(res_dir) / 'matlab'
+    save_dir.mkdir(parents=True, exist_ok=True)
 
-    amplitudes = safe_load_npy(res_dir / "amplitudes.npy")
-    channel_map = safe_load_npy(res_dir / "channel_map.npy")
-    channel_positions = safe_load_npy(res_dir / "channel_positions.npy")
-    channel_shanks = safe_load_npy(res_dir / "channel_shanks.npy")
-    cluster_Amplitude = safe_load_csv(res_dir / "cluster_Amplitude.tsv", sep="\t")
-    cluster_ContamPct = safe_load_csv(res_dir / "cluster_ContamPct.tsv", sep="\t")
-    cluster_group = safe_load_csv(res_dir / "cluster_group.tsv", sep="\t")
-    cluster_KSLabel = safe_load_csv(res_dir / "cluster_KSLabel.tsv", sep="\t")
-    kept_spikes = safe_load_npy(res_dir / "kept_spikes.npy")
-    ops = load_ops(res_dir / "ops.npy")
-    pc_features_ind = safe_load_npy(res_dir / "pc_features_ind.npy")
-    pc_features = safe_load_npy(res_dir / "pc_features.npy")
-    similar_templates = safe_load_npy(res_dir / "similar_templates.npy")
-    spike_clusters = safe_load_npy(res_dir / "spike_clusters.npy")
-    spike_detection_templates = safe_load_npy(res_dir / "spike_detection_templates.npy")
-    spike_positions = safe_load_npy(res_dir / "spike_positions.npy")
-    spike_templates = safe_load_npy(res_dir / "spike_templates.npy")
-    spike_times = safe_load_npy(res_dir / "spike_times.npy")
-    templates = safe_load_npy(res_dir / "templates.npy")
-    templates_ind = safe_load_npy(res_dir / "templates_ind.npy")
-    whitening_mat = safe_load_npy(res_dir / "whitening_mat.npy")
-    whitening_mat_data = safe_load_npy(res_dir / "whitening_mat_dat.npy")
-    whitening_mat_inv = safe_load_npy(res_dir / "whitening_mat_inv.npy")
+    amplitudes = safe_load_npy(ks_dir / "amplitudes.npy")
+    channel_map = safe_load_npy(ks_dir / "channel_map.npy")
+    channel_positions = safe_load_npy(ks_dir / "channel_positions.npy")
+    channel_shanks = safe_load_npy(ks_dir / "channel_shanks.npy")
+    cluster_Amplitude = safe_load_csv(ks_dir / "cluster_Amplitude.tsv", sep="\t")
+    cluster_ContamPct = safe_load_csv(ks_dir / "cluster_ContamPct.tsv", sep="\t")
+    cluster_group = safe_load_csv(ks_dir / "cluster_group.tsv", sep="\t")
+    cluster_KSLabel = safe_load_csv(ks_dir / "cluster_KSLabel.tsv", sep="\t")
+    kept_spikes = safe_load_npy(ks_dir / "kept_spikes.npy")
+    from kilosort.io import load_ops
+    ops = load_ops(ks_dir / "ops.npy")
+    pc_features_ind = safe_load_npy(ks_dir / "pc_feature_ind.npy")
+    pc_features = safe_load_npy(ks_dir / "pc_features.npy")
+    similar_templates = safe_load_npy(ks_dir / "similar_templates.npy")
+    spike_clusters = safe_load_npy(ks_dir / "spike_clusters.npy")
+    spike_detection_templates = safe_load_npy(ks_dir / "spike_detection_templates.npy")
+    spike_positions = safe_load_npy(ks_dir / "spike_positions.npy")
+    spike_templates = safe_load_npy(ks_dir / "spike_templates.npy")
+    spike_times = safe_load_npy(ks_dir / "spike_times.npy")
+    templates = safe_load_npy(ks_dir / "templates.npy")
+    templates_ind = safe_load_npy(ks_dir / "templates_ind.npy")
+    whitening_mat = safe_load_npy(ks_dir / "whitening_mat.npy")
+    whitening_mat_data = safe_load_npy(ks_dir / "whitening_mat_dat.npy")
+    whitening_mat_inv = safe_load_npy(ks_dir / "whitening_mat_inv.npy")
 
     cluster_snr = calc_snr(recording, templates)
     results_dict = {
@@ -52,71 +53,78 @@ def convert_to_matlab_files_ks4(res_dir: Path | str, recording):
         'spike_times': spike_times,
         'spike_positions': spike_positions,
         'pc_features': pc_features,
+        'pc_features_ind': pc_features_ind,
         'templates': templates,
+        'templates_ind': templates_ind,
         'similar_templates': similar_templates,
-        'cluster_KSLabel': cluster_KSLabel,
+        'cluster_label': cluster_KSLabel,
         'cluster_ContamPct': cluster_ContamPct,
         'cluster_Amplitude': cluster_Amplitude,
         'cluster_snr': cluster_snr,
         'spike_clusters': spike_clusters,
         'kept_spikes': kept_spikes,
-        'pc_features_ind': pc_features_ind,
-        'similar_templates': similar_templates,
-        'spike_templates': spike_templates,
-        'spike_positions': spike_positions,
         'spike_templates': spike_templates,
         'whitening_mat': whitening_mat,
         'whitening_mat_data': whitening_mat_data,
         'whitening_mat_inv': whitening_mat_inv,
     }
 
-    savemat(res_dir / 'results.mat', results_dict, long_field_names=True, format='5')
+    savemat(save_dir / 'results.mat', results_dict, long_field_names=True, format='5')
     replaced_ops = replace_none_recursive(ops)
-    savemat(res_dir / 'ops.mat', {'ops': replaced_ops}, long_field_names=True)
+    savemat(save_dir / 'ops.mat', {'ops': replaced_ops}, long_field_names=True)
 
 
-def convert_to_matlab_files(phy_dir: Path | str, recording):
+def convert_to_matlab_files(res_dir: Path | str, recording):
     """Phy フォルダ内のファイルを使って .mat を作成する関数。"""
 
-    phy_dir = Path(phy_dir)
+    phy_dir = Path(res_dir) / 'phy'
+    analyzer_dir = Path(res_dir) / 'analyzer' / 'extensions'
+    save_dir = Path(res_dir) / 'matlab'
+    save_dir.mkdir(parents=True, exist_ok=True)
 
     amplitudes = safe_load_npy(phy_dir / "amplitudes.npy")
-    channel_groups = safe_load_npy(phy_dir / "channel_groups.npy")
-    channel_map = safe_load_npy(phy_dir / "channel_map.npy")
-    channel_map_si = safe_load_npy(phy_dir / "channel_map_si.npy")
-    channel_positions = safe_load_npy(phy_dir / "channel_positions.npy")
-    cluster_channel_groups = safe_load_csv(phy_dir / "cluster_channel_group.tsv")
-    cluster_group = safe_load_csv(phy_dir / "cluster_group.tsv")
-    cluster_si_unit_ids = safe_load_csv(phy_dir / "cluster_si_unit_ids.tsv")
-    similar_templates = safe_load_npy(phy_dir / "similar_templates.npy")
-    spike_clusters = safe_load_npy(phy_dir / "spike_clusters.npy")
-    spike_templates = safe_load_npy(phy_dir / "spike_templates.npy")
     spike_times = safe_load_npy(phy_dir / "spike_times.npy")
+    spike_positions = safe_load_npy(analyzer_dir / 'spike_locations' / "spike_locations.npy")
+    pc_features = safe_load_npy(phy_dir / "pc_features.npy")
+    pc_features_ind = safe_load_npy(phy_dir / "pc_feature_ind.npy")
     templates = safe_load_npy(phy_dir / "templates.npy")
     templates_ind = safe_load_npy(phy_dir / "template_ind.npy")
+    similar_templates = safe_load_npy(phy_dir / "similar_templates.npy")
+    cluster_label = safe_load_csv(phy_dir / "cluster_label.tsv", sep="\t")
+    cluster_ContamPct = safe_load_csv(phy_dir / "cluster_contamPct.tsv", sep="\t")
+    cluster_Amplitude = safe_load_csv(phy_dir / "cluster_amplitude.tsv", sep="\t")
+    spike_clusters = safe_load_npy(phy_dir / "spike_clusters.npy")
+    spike_templates = safe_load_npy(phy_dir / "spike_templates.npy")
+    kept_spikes = safe_load_npy(phy_dir / "kept_spikes.npy")
+    whitening_mat = safe_load_npy(phy_dir / "whitening_mat.npy")
+    whitening_mat_data = safe_load_npy(phy_dir / "whitening_mat_dat.npy")
+    whitening_mat_inv = safe_load_npy(phy_dir / "whitening_mat_inv.npy")
 
     cluster_snr = calc_snr(recording, templates)
 
     results_dict = {
         'amplitudes': amplitudes,
-        'channel_groups': channel_groups,
-        'channel_map': channel_map,
-        'channel_map_si': channel_map_si,
-        'channel_positions': channel_positions,
-        'cluster_channel_groups': cluster_channel_groups,
-        'cluster_group': cluster_group,
-        'cluster_si_unit_ids': cluster_si_unit_ids,
-        'similar_templates': similar_templates,
-        'spike_clusters': spike_clusters,
-        'spike_templates': spike_templates,
         'spike_times': spike_times,
+        'spike_positions': spike_positions,
+        'pc_features': pc_features,
+        'pc_features_ind': pc_features_ind,
         'templates': templates,
         'templates_ind': templates_ind,
+        'similar_templates': similar_templates,
+        'cluster_label': cluster_label,
+        'cluster_ContamPct': cluster_ContamPct,
+        'cluster_Amplitude': cluster_Amplitude,
         'cluster_snr': cluster_snr,
+        'spike_clusters': spike_clusters,
+        'spike_templates': spike_templates,
+        'kept_spikes': kept_spikes,
+        'whitening_mat': whitening_mat,
+        'whitening_mat_data': whitening_mat_data,
+        'whitening_mat_inv': whitening_mat_inv,
     }
 
-    savemat(phy_dir / 'results.mat', results_dict, long_field_names=True, format='5')
-
+    savemat(save_dir / 'results.mat', results_dict, long_field_names=True, format='5')
+ 
 def replace_none_recursive(data):
     # 辞書の場合の処理
     if isinstance(data, dict):
--- myTools/read_simulation.py
+++ myTools/read_simulation.py
@@ -4,9 +4,15 @@ def get_simulation_path(dir_info):
     root_dir = dir_info["root_dir"]
     # name = dir_info["name"]
     ep = dir_info["ep"]
-    condition = dir_info["condition"]
+    cd = dir_info["cd"]
 
+    cars_dir = Path(root_dir) / ("ep"+ep) / ("cd"+cd) / "cars"
     dict_path = {
-        "exp": Path(root_dir) / name / exp_name / exp_name,
+        "exp": Path(root_dir) / ("ep"+ep) / ("cd"+cd),
+        "cars_dir": cars_dir,
+        # Recording.load_npz 用（settings/units/contacts 入り）。Recording.save_npz のデフォルトは recording.npz
+        "recording": cars_dir / "recording",
+        # SpikeInterface 用 ground truth sorting（別形式）
+        "ground_truth_sorting_npz": cars_dir / "ground_truth_sorting.npz",
     }
     return dict_path
